d016b3cd550f9d40a97416ff7a86ba8c5a326aeb
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-04-24 23:04:57 +0700

    `feat: updated global variables, types, and styles`

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hai there!
+    <div className="w-full h-36 bg-white flex items-center justify-center">
+        <span className="text-2xl font-semibold text-teal-500">Hai there!👋</span>
     </div>
   );
 }
