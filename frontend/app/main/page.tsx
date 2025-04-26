b5f310dd8dc8b2e870a298275752b4b29156894d
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-04-26 23:18:13 +0700

    feat: Add Fitur component with detailed features section; update Main component to include Fitur

--- frontend/app/main/page.tsx
+++ frontend/app/main/page.tsx
@@ -1,4 +1,5 @@
 import About from '@/components/landing/about'
+import Fitur from '@/components/landing/fitur'
 import Home from '@/components/landing/home'
 import React from 'react'
 
@@ -8,6 +9,7 @@ const Main = ({children} : {children: React.ReactNode}) => {
         <div className='overflow-x-hidden overflow-y-hidden'>
             <Home/>
             <About/>
+            <Fitur/>
             {children}
         </div>
     )
