8da212c728ae8204958bc84761ab5740ae48559d
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-04-26 22:46:46 +0700

    Add About component and update Home layout
    - Introduced a new About component to provide information about the organization.
    - Updated the Main component to include the About section alongside the Home section.
    - Modified Home component styles for better responsiveness and visual appeal.
    - Added a new SVG logo for branding purposes.

--- frontend/app/main/page.tsx
+++ frontend/app/main/page.tsx
@@ -1,12 +1,13 @@
+import About from '@/components/landing/about'
 import Home from '@/components/landing/home'
 import React from 'react'
 
 
 const Main = ({children} : {children: React.ReactNode}) => {
     return (
-        <div className='overflow-x-hidden overflow-y-auto'>
+        <div className='overflow-x-hidden overflow-y-hidden'>
             <Home/>
-            
+            <About/>
             {children}
         </div>
     )
