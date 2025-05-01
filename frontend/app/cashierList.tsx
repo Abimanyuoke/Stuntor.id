63c417cf53d8469fe0f19a2bfadbe083f8526ee3
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-05-01 07:53:54 +0700

    fix: Adjust icon size for Edukasi in CashierList component

--- frontend/app/cashierList.tsx
+++ frontend/app/cashierList.tsx
@@ -80,8 +80,8 @@ let CashierList: IPropMenu[] = [
         label: `Edukasi`,
         icon:
             <svg
-                width={23}
-                height={23}
+                width={25}
+                height={25}
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
