c157ed971a99ac20e453615ce92003d9d313fb22
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-05-17 21:18:01 +0700

    feat: Integrate react-slick for image carousel and update sidebar functionality

--- frontend/app/cashier/home/page.tsx
+++ frontend/app/cashier/home/page.tsx
@@ -3,12 +3,30 @@
 import React, { ReactNode } from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
+import Slider from "react-slick";
 
 const HomePage = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+
+        const settings = {
+            dots: true,
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: true,
+            speed: 2000,
+            autoplaySpeed: 2000,
+            cssEase: "linear"
+        };
+
+        return (
+            <div>
+                <Image
+                    src="/home.jpg"
+                    alt="Hero Image"
+                    width={1000}
+                    height={100}
+                    className=" object-cover"/>
+            </div>
+        );
+    }
 export default HomePage
